09b12308f5fbc83524fd87134d5631501fd68717
Hashfi Hazazi <muhamadhazazi52@gmail.com>
2024-11-11 08:40:20 +0700

    Beautify UI

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://jobexwtviwqscwtaorvi.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvYmV4d3R2aXdxc2N3dGFvcnZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgyNjI5MzMsImV4cCI6MjA0MzgzODkzM30.8d5FWnUjcf91W2od45E9iOz0ncTNAQGZc2FJwlWGOFA',
+    url: '',
+    anonKey: '',
   );
   final isLoggedIn = await isUsernameNull();
 
